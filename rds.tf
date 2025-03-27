658b476b640923b090601e54c894f914a50c1268
miguelrod15 <migueldevesar@gmail.com>
2025-03-27 23:13:20 +0000

    changed sg rules of tf best practices

---
+++ rds.tf
@@ -0,0 +1,32 @@
+# RDS Subnet Group (for private subnets)
+resource "aws_db_subnet_group" "wordpress_db_subnet_group" {
+  name       = "wordpress-db-subnet-group"
+  subnet_ids = [aws_subnet.private.id]
+
+  tags = {
+    Name = "wordpress-db-subnet-group"
+  }
+}
+
+# RDS MySQL Database Instance
+resource "aws_db_instance" "wordpress_db" {
+  identifier              = "wordpress-db"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t2.micro" # Free Tier eligible
+  username                = var.db_username
+  password                = random_password.db_password.result
+  db_name                 = var.db_name
+
+  db_subnet_group_name    = aws_db_subnet_group.wordpress_db_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  publicly_accessible     = false
+  skip_final_snapshot     = true
+  apply_immediately       = true
+
+  tags = {
+    Name = "wordpress-mysql"
+  }
+}
