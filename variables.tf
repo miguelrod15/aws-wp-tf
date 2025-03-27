e16b607b3aa2be80e70a036778e1f689c8ee95a2
miguelrod15 <migueldevesar@gmail.com>
2025-03-27 18:53:09 +0000

    added input variables for networking and RDS configuration

--- variables.tf
+++ variables.tf
@@ -1,7 +1,19 @@
+# General region (used in provider)
+variable "region" {
+  default = "eu-west-2"
+}
+
+# Availability zone (used in subnets)
+variable "availability_zone" {
+  default = "eu-west-2a"
+}
+
+# VPC CIDR
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
+# Subnet CIDRs
 variable "public_subnet_cidr" {
   default = "10.0.1.0/24"
 }
@@ -10,10 +22,11 @@ variable "private_subnet_cidr" {
   default = "10.0.2.0/24"
 }
 
-variable "availability_zone" {
-  default = "eu-west-2a"
+# RDS configuration
+variable "db_username" {
+  default = "admin"
 }
 
-variable "region" {
-  default = "eu-west-2"
+variable "db_name" {
+  default = "wordpress_db"
 }
