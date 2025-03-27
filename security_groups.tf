9ce16f912207b3de069c7bb5cf0ab8d90ed43182
miguelrod15 <migueldevesar@gmail.com>
2025-03-27 17:53:29 +0000

    changed SG rules based on Terraform best practices

--- security_groups.tf
+++ security_groups.tf
@@ -1,69 +1,82 @@
 # Security Group for EC2 (public instance)
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-sg"
-  description = "Allow HTTP, HTTPS, and SSH"
+  description = "Security group for EC2 instance"
   vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "Allow HTTP traffic"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTPS traffic"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow SSH access from my IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["81.84.124.177/32"]  # Replace with your real IP
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "ec2-sg"
   }
 }
 
+resource "aws_security_group_rule" "ec2_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow HTTP"
+}
+
+resource "aws_security_group_rule" "ec2_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow HTTPS"
+}
+
+resource "aws_security_group_rule" "ec2_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["81.84.124.177/32"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow SSH from my IP"
+}
+
+resource "aws_security_group_rule" "ec2_all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow all outbound traffic"
+}
+
+
 # Security Group for RDS (only accessible from EC2)
 resource "aws_security_group" "rds_sg" {
   name        = "rds-sg"
-  description = "Allow MySQL traffic from EC2 instance only"
+  description = "Security group for RDS"
   vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description     = "Allow MySQL from EC2"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "rds-sg"
   }
+}
+
+resource "aws_security_group_rule" "rds_mysql" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.ec2_sg.id
+  security_group_id        = aws_security_group.rds_sg.id
+  description              = "Allow MySQL from EC2"
+}
+
+resource "aws_security_group_rule" "rds_all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds_sg.id
+  description       = "Allow all outbound traffic"
 }
