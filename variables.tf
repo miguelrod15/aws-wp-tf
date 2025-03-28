e59ff745e114359a92d05857974f454830d9f8ec
miguelrod15 <migueldevesar@gmail.com>
2025-03-28 13:51:47 +0000

    updated variable definitions

--- variables.tf
+++ variables.tf
@@ -18,9 +18,14 @@ variable "public_subnet_cidr" {
   default = "10.0.1.0/24"
 }
 
-variable "private_subnet_cidr" {
-  default = "10.0.2.0/24"
+variable "private_subnet_cidr_a" {
+  default = "10.0.3.0/24"
 }
+
+variable "private_subnet_cidr_b" {
+  default = "10.0.4.0/24"
+}
+
 
 # RDS configuration
 variable "db_username" {
