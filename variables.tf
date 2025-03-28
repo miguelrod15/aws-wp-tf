4dbb817e354ff19c65e5ef3030514a1496ddef83
miguelrod15 <migueldevesar@gmail.com>
2025-03-28 19:57:48 +0000

    converted public subnet CIDR and AZ to Terraform input variables for flexibility

--- variables.tf
+++ variables.tf
@@ -15,8 +15,15 @@ variable "vpc_cidr" {
 
 # Subnet CIDRs
 variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"
+  description = "CIDR block for the public subnet"
+  default     = "10.0.1.0/24"
 }
+
+variable "public_subnet_az" {
+  description = "Availability Zone for the public subnet"
+  default     = "eu-west-2a"
+}
+
 
 variable "private_subnet_cidr_a" {
   default = "10.0.3.0/24"
