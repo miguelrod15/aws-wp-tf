7e784112205bec7901a68fa02e62561f1625b5a2
miguelrod15 <migueldevesar@gmail.com>
2025-04-01 16:20:31 +0100

    fix: set AWS_REGION from secret

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -4,10 +4,10 @@ resource "aws_cloudwatch_dashboard" "ec2_dashboard" {
   dashboard_body = jsonencode({
     widgets = [
       {
-        type = "metric",
-        x = 0,
-        y = 0,
-        width = 12,
+        type   = "metric",
+        x      = 0,
+        y      = 0,
+        width  = 12,
         height = 6,
         properties = {
           metrics = [
@@ -15,85 +15,85 @@ resource "aws_cloudwatch_dashboard" "ec2_dashboard" {
             ["CWAgent", "disk_used_percent", "InstanceId", aws_instance.wordpress.id],
             ["AWS/EC2", "CPUUtilization", "InstanceId", aws_instance.wordpress.id]
           ],
-          view = "timeSeries",
+          view    = "timeSeries",
           stacked = false,
-          region = var.region,
-          title = "EC2 - Custom Metrics"
+          region  = var.region,
+          title   = "EC2 - Custom Metrics"
         }
       }
     ]
   })
-}  
+}
 
 resource "aws_cloudwatch_dashboard" "rds_dashboard" {
   dashboard_name = "RDS-Dashboard"
   dashboard_body = jsonencode({
     widgets = [
       {
-        type = "metric",
-        x = 0,
-        y = 0,
-        width = 12,
+        type   = "metric",
+        x      = 0,
+        y      = 0,
+        width  = 12,
         height = 6,
         properties = {
-          title = "RDS CPU Utilization",
-          view = "timeSeries",
+          title  = "RDS CPU Utilization",
+          view   = "timeSeries",
           region = var.region,
           metrics = [
-            [ "AWS/RDS", "CPUUtilization", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id ]
+            ["AWS/RDS", "CPUUtilization", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id]
           ],
-          stat = "Average",
+          stat   = "Average",
           period = 300
         }
       },
       {
-        type = "metric",
-        x = 12,
-        y = 0,
-        width = 12,
+        type   = "metric",
+        x      = 12,
+        y      = 0,
+        width  = 12,
         height = 6,
         properties = {
-          title = "RDS Database Connections",
-          view = "timeSeries",
+          title  = "RDS Database Connections",
+          view   = "timeSeries",
           region = var.region,
           metrics = [
-            [ "AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id ]
+            ["AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id]
           ],
-          stat = "Average",
+          stat   = "Average",
           period = 300
         }
       },
       {
-        type = "metric",
-        x = 0,
-        y = 6,
-        width = 12,
+        type   = "metric",
+        x      = 0,
+        y      = 6,
+        width  = 12,
         height = 6,
         properties = {
-          title = "RDS Freeable Memory",
-          view = "timeSeries",
+          title  = "RDS Freeable Memory",
+          view   = "timeSeries",
           region = var.region,
           metrics = [
-            [ "AWS/RDS", "FreeableMemory", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id ]
+            ["AWS/RDS", "FreeableMemory", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id]
           ],
-          stat = "Average",
+          stat   = "Average",
           period = 300
         }
       },
       {
-        type = "metric",
-        x = 12,
-        y = 6,
-        width = 12,
+        type   = "metric",
+        x      = 12,
+        y      = 6,
+        width  = 12,
         height = 6,
         properties = {
-          title = "RDS Free Storage Space",
-          view = "timeSeries",
+          title  = "RDS Free Storage Space",
+          view   = "timeSeries",
           region = var.region,
           metrics = [
-            [ "AWS/RDS", "FreeStorageSpace", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id ]
+            ["AWS/RDS", "FreeStorageSpace", "DBInstanceIdentifier", aws_db_instance.wordpress_db.id]
           ],
-          stat = "Average",
+          stat   = "Average",
           period = 300
         }
       }
